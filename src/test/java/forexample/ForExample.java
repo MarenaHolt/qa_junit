785839b5aa0ed14bc0ab78e41f98d703126b801e
Marena <fabriana.sands@gmail.com>
2023-07-05 23:03:43 +0500

    add examples

---
+++ src/test/java/forexample/ForExample.java
@@ -0,0 +1,21 @@
+package forexample;
+
+public class ForExample {
+    //    static List<String> stringList() {
+//        return $$(".ShowCol-title").texts();
+//      //  return $$(selector).texts();
+//    }
+//
+//    @Test
+//            void test() {
+//        open("https://myshows.me/ratings/top/?mode=f");
+//        System.out.println(stringList());
+//        List<String>sss = stringList();
+//        List<String> names = new ArrayList<>();
+//        for(int i = 0; i < sss.size(); i++) {
+//            names.add("\"" + sss.get(i) + "\"");
+//        }
+//
+//        System.out.println(names.toString());
+//    }
+}
